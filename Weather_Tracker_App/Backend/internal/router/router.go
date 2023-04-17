23285521a8586d59432ec8c9c3a5f75442904b3f
Jayaprakash Aluri <alurijayaprakash@gmail.com>
2023-04-17 22:56:39 +0530

    WeatherTracker App Back/Frontend Completed

--- Weather_Tracker_App/Backend/internal/router/router.go
+++ Weather_Tracker_App/Backend/internal/router/router.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 )
 
+const APIKEY = "b3740c800de5629c9096b5f2068d74b5"
+
 func InitRouter() {
 	// var r = mux.NewRouter() // router by mux
 	http.HandleFunc("/hello", HelloHandler)
@@ -18,9 +20,12 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func TrackHandler(w http.ResponseWriter, r *http.Request) {
-	CityName := r.URL.Query().Get("q")  // city name
-	Appid := r.URL.Query().Get("appid") // appid
-
+	CityName := r.URL.Query().Get("q") // city name
+	// Appid := r.URL.Query().Get("appid") // appid // // localhost:8080/track?q=Mumbai&appid=1f485364c2c92f74eaca9d0bb5768b5f
+	Appid := APIKEY // // localhost:8080/track?q=Mumbai
+	//Allow CORS here By * or specific origin
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
 	fmt.Println(CityName, Appid)
 	data, err := app.QueryOpenWeatherAPI(CityName, Appid)
 	if err != nil {
